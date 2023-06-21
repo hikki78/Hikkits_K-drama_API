f797221ce25a693217970cf931a959d7b5559b1c
Meghadri <79590183+hikki78@users.noreply.github.com>
2023-06-21 19:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,59 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-#GET
-@app.route('/api/kdramas', methods=['GET'])
+# Dummy Kdrama data
+kdramas = [
+    {"id": 1, "title": "Crash Landing on You", "genre": "Romance"},
+    {"id": 2, "title": "Goblin", "genre": "Fantasy"},
+    {"id": 3, "title": "Reply 1988", "genre": "Drama"}
+]
+
+# Route to retrieve all Kdramas
+@app.route('/kdramas', methods=['GET'])
 def get_kdramas():
-    kdramas = [
-        {'id': 1, 'title': 'Crash Landing on You', 'year': 2019},
-        {'id': 2, 'title': 'Descendants of the Sun', 'year': 2016},
-        {'id': 3, 'title': 'Goblin', 'year': 2016},
-        # Add more K-dramas as needed
-    ]
     return jsonify(kdramas)
 
-#POST
-@app.route('/api/kdramas', methods=['POST'])
-def add_kdrama():
-    kdrama = {
-        'id': request.json['id'],
+# Route to retrieve a specific Kdrama by ID
+@app.route('/kdramas/<int:kdrama_id>', methods=['GET'])
+def get_kdrama(kdrama_id):
+    kdrama = next((kdrama for kdrama in kdramas if kdrama['id'] == kdrama_id), None)
+    if kdrama:
+        return jsonify(kdrama)
+    else:
+        return jsonify({"message": "Kdrama not found"}), 404
+
+# Route to create a new Kdrama
+@app.route('/kdramas', methods=['POST'])
+def create_kdrama():
+    new_kdrama = {
+        'id': len(kdramas) + 1,
         'title': request.json['title'],
-        'year': request.json['year']
+        'genre': request.json['genre']
     }
-    # Perform additional validation or data processing if needed
-    # Add the new K-drama to the data source (e.g., database)
-    return jsonify(kdrama), 201
+    kdramas.append(new_kdrama)
+    return jsonify(new_kdrama), 201
 
-#Upadte operation 
-@app.route('/api/kdramas/<int:kdrama_id>', methods=['PUT'])
+# Route to update an existing Kdrama
+@app.route('/kdramas/<int:kdrama_id>', methods=['PUT'])
 def update_kdrama(kdrama_id):
-    for kdrama in kdramas:
-        if kdrama['id'] == kdrama_id:
-            kdrama['title'] = request.json['title']
-            kdrama['year'] = request.json['year']
-            return jsonify(kdrama)
-    return jsonify({'message': 'K-drama not found'}), 404
+    kdrama = next((kdrama for kdrama in kdramas if kdrama['id'] == kdrama_id), None)
+    if kdrama:
+        kdrama['title'] = request.json['title']
+        kdrama['genre'] = request.json['genre']
+        return jsonify(kdrama)
+    else:
+        return jsonify({"message": "Kdrama not found"}), 404
 
-#Delete operation 
-@app.route('/api/kdramas/<int:kdrama_id>', methods=['DELETE'])
+# Route to delete an existing Kdrama
+@app.route('/kdramas/<int:kdrama_id>', methods=['DELETE'])
 def delete_kdrama(kdrama_id):
-    for kdrama in kdramas:
-        if kdrama['id'] == kdrama_id:
-            kdramas.remove(kdrama)
-            return jsonify({'message': 'K-drama deleted'})
-    return jsonify({'message': 'K-drama not found'}), 404
+    kdrama = next((kdrama for kdrama in kdramas if kdrama['id'] == kdrama_id), None)
+    if kdrama:
+        kdramas.remove(kdrama)
+        return jsonify({"message": "Kdrama deleted"})
+    else:
+        return jsonify({"message": "Kdrama not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8000)
+    app.run(host='0.0.0.0', port=5000)
